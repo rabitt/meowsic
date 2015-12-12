bb284f7827a4da076c522e05ee10d052a69c9b59
Rachel Bittner <rachelbittner@spotify.com>
2015-12-12 18:11:20 -0500

    meow track yay

--- meowsic/audio_backend.py
+++ meowsic/audio_backend.py
@@ -1,5 +1,4 @@
 import librosa
-import sox
 import os
 import numpy as np
 
@@ -9,12 +8,10 @@ MEDMEOW = os.path.join(MEOWP3S, "med_meow.wav")
 LOWMEOW = os.path.join(MEOWP3S, "low_meow.wav")
 
 
-def get_beats(audio_path):
-    y, fs = librosa.load(audio_path)
+def get_beats(y, fs):
     _, beats = librosa.beat.beat_track(y=y, sr=fs)
-    audio_length = len(y)
     beats_in_seconds = librosa.frames_to_time(beats, sr=fs)
-    return beats_in_seconds, audio_length, fs
+    return beats_in_seconds
 
 
 def make_meow_track(beats, audio_length, fs):
@@ -27,21 +24,34 @@ def make_meow_track(beats, audio_length, fs):
     low_len = len(low_meow)
 
     bass_drum = np.zeros((audio_length, ))
+    snare_drum = np.zeros((audio_length, ))
+    high_drum = np.zeros((audio_length, ))
 
-    print beats
     beats_in_samples = np.round(beats*fs)
-    print beats_in_samples
 
     offset = int(low_len*0.75)
-    for i in beats_in_samples:
-        bass_drum[int(i)-offset: int(i)+low_len-offset] = low_meow
+    for i in beats_in_samples[::2]:
+        if int(i)-offset >= 0 and int(i)+low_len-offset < audio_length:
+            bass_drum[int(i)-offset: int(i)+low_len-offset] = low_meow
 
-    
+    offset = 0
+    for i in beats_in_samples[1::2]:
+        if int(i)-offset >= 0 and int(i)+med_len-offset < audio_length:
+            snare_drum[int(i)-offset: int(i)+med_len-offset] = med_meow
 
-    # librosa.output(bass_drum, "/Users/rachelbittner/Desktop/test.wav")
-    librosa.output.write_wav(
-        "/Users/rachelbittner/Desktop/test.wav", bass_drum, fs)
-    return bass_drum
+    offset = int(high_len*0)
+    offbeats_in_samples = (beats_in_samples[:-1] + beats_in_samples[1:])/2
+    for i in offbeats_in_samples:
+        if int(i)-offset >= 0 and int(i)+high_len-offset < audio_length:
+            high_drum[int(i)-offset: int(i)+high_len-offset] = high_meow
 
-def add_meow_track_to_audio(audio_path, meow_track_path, output_audio):
-    sox.mix([meow_track_path, audio_path], output_audio)
+    return bass_drum + snare_drum + high_drum
+
+
+def add_meow_track_to_audio(audio_path, output_path):
+    y, fs = librosa.load(audio_path)
+    audio_length = len(y)
+    beats_in_seconds = get_beats(y, fs)
+    meow_track = make_meow_track(beats_in_seconds, audio_length, fs)
+    mix = y/np.max(y) + 0.5*meow_track/np.max(meow_track)
+    librosa.output.write_wav(output_path, mix, fs)
